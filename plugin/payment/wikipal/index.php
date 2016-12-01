cf7f0dd6d9a49d702a6d02a0e5077ef16da279f4
ltiny <lizard.tiny@gmail.com>
2016-12-01 20:40:42 +0330

    WikiPal - MegaPlus ( https://wikiservice.co/vip ) Plugin

---
+++ plugin/payment/wikipal/index.php
@@ -0,0 +1,12 @@
+<?php //004fb
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+?>
+HR+cPuDvw0sgtz0YiyDzBfhSf2dun13GC6ClUk9aK9J64gvbuH2ib6aEReZ/ZsGKR4s77j4DfChi
+ARB6eW9tRnK3wi1Pscx+ZW09i5WHWg6I89JaP7sMPgNiZkkqC4SD4WU1I5zlfRAMgtMRN7g0nsEx
+1zrYX5u+usTehYb5ceDPMh9J4zwRBFp3SjhAjYQ3fvdBkFE1nOzndhRO/B8dc57JgH+jsYzldwgZ
+P0xG6UP5jFL8ea58T8Mx2lLcVyUEygNQeCkKZSFpShjpWsY5X4eAXu/0NwdIRQ92Ux0Rc3rpqWmj
+xvZ35rp58GOBkK53VGBKFvcfg84N3Crg+u3z1tylgjzGSjQCaO6yjUrxXYvyg66SimZZ0P9z+BGh
+SCs1OSs0uec0ukkyniMvQvX+lJDXYMwT6wQU1kc9ZxLSp154qMbBtvSH3GPYa0ZZlrIDcrul3MbH
+2LjjYtg9bhZvlMntAYZIICM+NzSjhLS59IG7plEJCI9lwpe1paN6elvN1jQUvc4Y1sgIV0zGJMz7
+IEqKjvKTpmkVVdSFpd5hnQQKTN/9oLob4e5QT1rRnMdWeWkp//fIUdtG1fpuT6mFNPaWKEdaJQdq
+GA0fRbNM
